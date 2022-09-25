e1ef3c0857911ece079792fd108ff6316a2e134b
unknowns <unknowns0074@gmail.com>
2022-09-25 15:35:26 -0300

    Adding gorm libs

---
+++ src/libs/mysql.go
@@ -0,0 +1,34 @@
+package libs
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type DbConfig struct {
+	Host     string
+	Port     string
+	Database string
+	User     string
+	Password string
+	Charset  string
+}
+
+func (c *DbConfig) InitMysqlDB() *gorm.DB {
+	connString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local", c.User, c.Password, c.Host, c.Port, c.Database, c.Charset)
+	db, err := gorm.Open(mysql.Open(connString), &gorm.Config{})
+	if err != nil {
+		log.Panic(err)
+		os.Exit(-1)
+	}
+
+	//db.AutoMigrate(&models.App{}, &models.UserLicense{})
+
+	return db
+}
